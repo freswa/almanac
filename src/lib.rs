3cb2a06d610d4e8121449ab09210c0f9a7414860
Ruben Pollan <meskio@sindominio.net>
2018-11-26 13:10:38 -0600

    Convert the project into a lib + main

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+extern crate ical;
+extern crate chrono;
+extern crate chrono_tz;
+
+mod date;
+mod event;
+mod periodic;
+mod calendar;
+mod errors;
+
+pub use calendar::Calendar;
+pub use date::Date;
+pub use chrono::Duration;
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-extern crate ical;
-extern crate chrono;
-extern crate chrono_tz;
-
-mod date;
-mod event;
-mod periodic;
-mod calendar;
-mod errors;
+extern crate almanac;
 
 use std::env;
 use std::io::BufReader;
 use std::fs::File;
-use chrono::Duration;
-use date::Date;
-use calendar::Calendar;
+use almanac::Duration;
+use almanac::Date;
+use almanac::Calendar;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let file = File::open(&args[1]).unwrap();
     let buf = BufReader::new(file);
     let calendar = Calendar::parse(buf).unwrap();
-    println!("{}", calendar);
-    println!("");
 
     let now = Date::now();
-    let events = calendar.get(&now, &(now + Duration::weeks(10)));
+    let events = calendar.get(&now, &(now + Duration::days(1)));
     for e in events {
         println!("{}", e);
     }
